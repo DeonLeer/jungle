b515e46babd4dfd605c619bd251c0f3317c5d457
deonleer <deon.leer96@gmail.com>
2021-02-07 23:42:51 +0000

    added about us page

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
     resources :categories, only: [:index]
   end
 
+  get 'about' => 'about#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
